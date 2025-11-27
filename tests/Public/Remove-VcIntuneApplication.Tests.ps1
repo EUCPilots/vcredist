70544e4c876a74a248225da7fbdf207175991a3c
Aaron Parker <aaron@stealthpuppy.com>
2025-11-27 14:48:18 +1100

    Update module version

--- tests/Public/Remove-VcIntuneApplication.Tests.ps1
+++ tests/Public/Remove-VcIntuneApplication.Tests.ps1
@@ -27,38 +27,38 @@ Describe "Remove-VcIntuneApplication" -Skip:$Skip {
         }
     }
 
-    Context "Import-VcIntuneApplication imports VcRedists into a target tenant" {
-        BeforeAll {
-            foreach ($Module in @("MSAL.PS", "IntuneWin32App")) {
-                Install-Module -Name $Module -Force
-            }
+    # Context "Import-VcIntuneApplication imports VcRedists into a target tenant" {
+    #     BeforeAll {
+    #         foreach ($Module in @("MSAL.PS", "IntuneWin32App")) {
+    #             Install-Module -Name $Module -Force
+    #         }
 
-            try {
-                # Authenticate to the Graph API
-                # Expects secrets to be passed into environment variables
-                Write-Information -MessageData "Authenticate to the Graph API"
-                $params = @{
-                    TenantId     = "$env:TENANT_ID"
-                    ClientId     = "$env:CLIENT_ID"
-                    ClientSecret = "$env:CLIENT_SECRET"
-                }
-                $script:AuthToken = Connect-MSIntuneGraph @params
-            }
-            catch {
-                throw $_
-            }
+    #         try {
+    #             # Authenticate to the Graph API
+    #             # Expects secrets to be passed into environment variables
+    #             Write-Information -MessageData "Authenticate to the Graph API"
+    #             $params = @{
+    #                 TenantId     = "$env:TENANT_ID"
+    #                 ClientId     = "$env:CLIENT_ID"
+    #                 ClientSecret = "$env:CLIENT_SECRET"
+    #             }
+    #             $script:AuthToken = Connect-MSIntuneGraph @params
+    #         }
+    #         catch {
+    #             throw $_
+    #         }
 
-            New-Item -Path "$env:RUNNER_TEMP\Deployment" -ItemType Directory -Force | Out-Null
-            $Path = "$env:RUNNER_TEMP\Deployment"
-            $SavedVcRedist = Save-VcRedist -Path $Path -VcList (Get-VcList -Release "2017" )
-            Import-VcIntuneApplication -VcList $SavedVcRedist | Out-Null
-            Start-Sleep -Seconds 5 # Wait for Intune to process the import
-        }
+    #         New-Item -Path "$env:RUNNER_TEMP\Deployment" -ItemType Directory -Force | Out-Null
+    #         $Path = "$env:RUNNER_TEMP\Deployment"
+    #         $SavedVcRedist = Save-VcRedist -Path $Path -VcList (Get-VcList -Release "2017" )
+    #         Import-VcIntuneApplication -VcList $SavedVcRedist | Out-Null
+    #         Start-Sleep -Seconds 5 # Wait for Intune to process the import
+    #     }
 
-        It "Removes VcRedist from Intune OK" {
-            { Remove-VcIntuneApplication -VcList $SavedVcRedist -Confirm:$false } | Should -Not -Throw
-        }
-    }
+    #     It "Removes VcRedist from Intune OK" {
+    #         { Remove-VcIntuneApplication -VcList $SavedVcRedist -Confirm:$false } | Should -Not -Throw
+    #     }
+    # }
 
     # Context "ShouldProcess support" {
     #     It "Should honor ShouldProcess and not call Remove-IntuneWin32App if ShouldProcess returns false" {
